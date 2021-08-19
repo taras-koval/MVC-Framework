99bb7f5e7e4e5a1f846991419dcb5ebe86c48492
Taras <tkoval37@gmail.com>
2021-08-19 23:09:29 +0300

    Update helper functions

--- src/helpers.php
+++ src/helpers.php
@@ -2,7 +2,9 @@
 
 use App\Core\App;
 use App\Core\Database;
+use App\Core\Response;
 use App\Core\Session;
+use App\Core\View;
 
 function db(): Database
 {
@@ -13,3 +15,17 @@ function session(): Session
 {
     return App::$session;
 }
+
+function view(string $path, array $data = []) : Response
+{
+    $view = new View();
+    $views = $view->getViewsPath();
+    $renderedView = $view->make("$views/$path", $data);
+    return new Response($renderedView);
+}
+
+function redirect($url)
+{
+    (new Response())->redirect($url);
+}
+
