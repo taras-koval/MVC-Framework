4300982832ea4d91a51da4049b1318c40b7db4c7
Taras <tkoval37@gmail.com>
2021-08-19 23:06:06 +0300

    Rename layouts

--- views/_layouts/main.php
+++ views/_layouts/main.php
@@ -1,91 +1,65 @@
-<?php
-
-use App\Core\App;
-
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zxx">
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
     
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-          crossorigin="anonymous">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Taras Koval">
     
-    <title>Hello, world!</title>
+    <title>Test</title>
+    
+    <!--<link rel="stylesheet" href="../../public/css/fonts.css">-->
+    <link rel="stylesheet" href="../../public/css/common.css">
+    <link rel="stylesheet" href="../../public/css/main.css">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;700;800;900&display=swap" rel="stylesheet">
+
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<header class="page-header">
     <div class="container">
-        <!--<div class="container-fluid">-->
-        <!--<a class="navbar-brand" href="#">Navbar</a>-->
         
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown"
-                aria-expanded="false"
-                aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <nav class="left-page-nav">
+            
+            <a href="/" class="logo-link">Home</a>
+            
+            <div class="nav-group">
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+            </div>
+            
+        </nav>
         
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <nav class="right-page-nav">
             
-            <ul class="navbar-nav">
-                
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
-                    <!--<a class="nav-link active" aria-current="page" href="/">Home</a>-->
-                </li>
-                
-                <li class="nav-item">
-                    <a class="nav-link" href="/contact">Contact</a>
-                </li>
+            <div class="nav-group">
+                <a href="/login/">Login</a>
+                <a href="/register/">Register</a>
+            </div>
             
-            </ul>
-            
-            <ul class="navbar-nav" style="margin-left: auto">
-                
-                <li class="nav-item">
-                    <a class="nav-link" href="/login">Login</a>
-                </li>
-                
-                <li class="nav-item">
-                    <a class="nav-link" href="/register">Register</a>
-                </li>
-            
-            </ul>
-        
-        </div>
-        <!--</div>-->
+        </nav>
+    
     </div>
-</nav>
+</header>
 
-<div class="container">
+<main class="page-main">
+    <div class="container">
+        {{content}}
+    </div>
+</main>
+
+<!--<footer class="page-footer">
+    <div class="container">
     
-    <?php if (App::$session->getFlash('success')): ?>
-        <div class="alert alert-success">
-            <?= App::$session->getFlash('success') ?>
-        </div>
-    <?php endif; ?>
-    
-    {{content}}
-</div>
+    </div>
+</footer>-->
 
-<!-- Optional JavaScript; choose one of the two! -->
+<script src="/public/js/main.js"></script>
 
-<!-- Option 1: Bootstrap Bundle with Popper -->
-<!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>-->
-
-<!-- Option 2: Separate Popper and Bootstrap JS -->
-<!--
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
--->
 </body>
 </html>
