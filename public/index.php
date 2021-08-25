bada703fc8cf07d98ac1c2bc45e8022dc89ba1ac
Taras <tkoval37@gmail.com>
2021-08-25 20:26:41 +0300

    Add config for App errors display

--- public/index.php
+++ public/index.php
@@ -3,10 +3,17 @@
 use App\Core\App;
 use Symfony\Component\Dotenv\Dotenv;
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
 define('ROOT', dirname(__DIR__));
+
+ini_set('display_errors', 0);
+error_reporting(0);
+
+if ((require ROOT.'/config/app.php')['errors']) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
 require ROOT.'/vendor/autoload.php';
 require ROOT.'/src/helpers.php';
 
