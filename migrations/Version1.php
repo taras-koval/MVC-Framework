eef65e6283a293be6365984af050e636a690b7dd
Taras <tkoval37@gmail.com>
2021-09-15 06:05:20 +0300

    Update migration

--- migrations/Version1.php
+++ migrations/Version1.php
@@ -1,16 +1,19 @@
 <?php
 
+use App\Core\App;
+
 class Version1
 {
     public function up()
     {
-        $db = \App\Core\App::$database;
+        $db = App::$database;
         $sql = <<<SQL
             CREATE TABLE users (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 username VARCHAR(255) NOT NULL,
                 email VARCHAR(255) NOT NULL,
                 password VARCHAR(255) NOT NULL,
+                status TINYINT DEFAULT 1 NOT NULL,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             ) ENGINE=INNODB;
         SQL;
@@ -19,7 +22,7 @@ class Version1
     
     public function down()
     {
-        $db = \App\Core\App::$database;
+        $db = App::$database;
         $sql = <<<SQL
             DROP TABLE users;
         SQL;
